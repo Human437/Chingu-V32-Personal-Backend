5eff0cea484e607ac0827e1bcb4bd841f84ea53b
Ryan <leeyuenhigh@gmail.com>
2021-08-16 07:21:53 -0400

    Inital commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const authenticationRoute = require("./src/routes/authentication");
+const userFunctionsRoute = require("./src/routes/userFunctions");
+const productRoutes = require("./src/routes/productRoutes");
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.use(helmet());
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    // Line below tells mongoose to use MongoDb driver's findOneAndUpdate() instead of the legacy version that will be removed after Mongoose 6.0
+    useFindAndModify: false,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authenticationRoute);
+app.use("/api/userFunctions", userFunctionsRoute);
+app.use("/api/productRoutes", productRoutes);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../frontend/build"));
+// }
+
+app.listen(PORT, () => {
+  console.log(`Backend is running on ${PORT}`);
+});
---
+++ src/routes/authentication.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const AuthService = require("./auth-service");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    for (const field of ['username', 'email', 'password'])
+      if (!req.body[field])
+        return res.status(400).json(
+          `missing ${field}`
+        )
+
+    const hashedPass = await AuthService.hashPassword(password)
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass
+    });
+
+    // checks for duplicate username
+    const usernameCheck = await User.findOne({ username: req.body.username });
+    if (usernameCheck) return res.status(400).json("username already registered");
+
+    // checks for duplicate email
+    const emailCheck = await User.findOne({ email: req.body.email });
+    if (emailCheck) return res.status(400).json("email already registered");
+
+    const passwordError = AuthService.validatePassword(username, email, password)
+    if (passwordError) return res.status(400).json(passwordError)
+
+    const user = await newUser.save();
+    
+    const sub = username
+    const payload = { user_id: User._id }
+    res.send({
+      authToken: AuthService.createJwt(sub, payload),
+    })
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+
+    const username = req.body.username;
+    // const password = req.body.password;
+
+    for (const field of ['username', 'password'])
+      if (!req.body[field])
+        return res.status(400).json(
+          `missing ${field}`
+        )
+
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(401).json("Wrong credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) return res.status(401).json("Wrong credentials!");
+
+    // const { password, paymentInfo, ...info } = user._doc;
+    // res.status(200).json(info);
+
+    const sub = username
+    const payload = { user_id: User._id }
+    res.send({
+      authToken: AuthService.createJwt(sub, payload),
+    })
+
+  } catch (err) {
+    res.status(401).json(err);
+  }
+});
+
+module.exports = router;
